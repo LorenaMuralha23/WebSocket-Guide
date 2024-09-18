3545fce14ec2c68f87ba5e810d4564dec49cbc8b
Lorena Muralha <mlmuralha2301@gmail.com>
2024-09-18 16:48:29 -0300

    project created

---
+++ src/main/java/com/example/guide_websocket/GuideWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.guide_websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuideWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/guide_websocket/GuideWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guide_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
